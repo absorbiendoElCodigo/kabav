1b4f7d1790ae52d659fb2edcff046d63c782f1c9
absorbiendoElCodigo <32875385+absorbiendoElCodigo@users.noreply.github.com>
2017-10-27 10:56:05 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        <?php
+            $titleIndex = 'Kabav - Pagina principal';
+            echo $titleIndex;
+        ?>
+    </title>
+    <link href="favicon.ico" rel="shortcut icon" />
+    <link href="basic.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div id="background">
+        <div id="table1">
+            <div id="windows" >
+                
+            </div>
+        </div>
+        <div id="table2">
+            <div id="opt1" onmouseover="this.style.backgroundColor='rgba(185, 40, 40, 0.3)';" onmouseout="this.style.backgroundColor='transparent';" >
+                <img src="https://d30y9cdsu7xlg0.cloudfront.net/png/178625-200.png" style="width:100%;height:90%"/>
+            </div>
+            <div id="opt2" onmouseover="this.style.backgroundColor='rgba(185, 40, 40, 0.3)';" onmouseout="this.style.backgroundColor='transparent';" >
+                <img src="images/info.png" style="width:100%;height:90%"/>
+            </div>
+            <div id="opt3" onmouseover="this.style.backgroundColor='rgba(185, 40, 40, 0.3)';" onmouseout="this.style.backgroundColor='transparent';" >
+                <img src="https://dxj7eshgz03ln.cloudfront.net/production/publication/logo/1883/e2450b86-d71a-4a53-9a93-4129358cd46e.png" style="width:100%;height:90%"/>
+            </div>
+            <div id="opt4" onmouseover="this.style.backgroundColor='rgba(185, 40, 40, 0.3)';" onmouseout="this.style.backgroundColor='transparent';" >
+                <img src="https://furtaev.ru/preview/social_connections.png" style="width:100%;height:90%"/>
+            </div>
+            <div id="opt5" onmouseover="this.style.backgroundColor='rgba(185, 40, 40, 0.3)';" onmouseout="this.style.backgroundColor='transparent';" >
+                <img src="https://dxj7eshgz03ln.cloudfront.net/production/publication/logo/1883/e2450b86-d71a-4a53-9a93-4129358cd46e.png" style="width:100%;height:90%"/>
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
